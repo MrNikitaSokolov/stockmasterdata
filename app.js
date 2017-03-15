d2cb6a1ae50c566afbad2c7c15cb15dc67088803
Nikita Sokolov <nsokolo@uwo.ca>
2017-03-14 23:18:13 -0400

    Data for two companies and serving the data via HTTP

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-
-// create a new express server
+var companiesRepo = require('./companiesInfo');
 var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+});
+
+app.get('/company', function(req, res){
+    res.send(Object.keys(companiesRepo.companies));
+});
+
+app.get('/company/:name', function(req, res) {
+    var name = req.params.name;
+    res.send(companiesRepo.companies[name]);
 });
